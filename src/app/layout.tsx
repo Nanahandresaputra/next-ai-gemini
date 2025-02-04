a8fe5e08030efc7a83f5a29a9878096cc4e2d31a
nanahandresaputra <nanaggmu@gmail.com>
2025-02-04 18:35:01 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
-import robot from '../assets/robot.gif';
+// import Image from "next/image";
+// import robot from '../assets/robot.gif';
 
 
 const geistSans = Geist({
