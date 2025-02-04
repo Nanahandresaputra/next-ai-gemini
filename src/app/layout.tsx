0a564733ea4ac5896dde3c6e905f8195fd294bb3
nanahandresaputra <nanaggmu@gmail.com>
2025-02-04 17:36:14 +0700

    themme dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import robot from '../assets/robot.gif';
+import { ThemeProvider } from "next-themes";
 
 
 const geistSans = Geist({
@@ -25,14 +26,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en" className="dark" >
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <section className="grid grid-cols-7">
           <div className="w-full border-r-gray-600 border-r-2 rounded-r-3xl h-screen flex flex-col justify-between py-14  col-span-1">
-            <p className="text-center font-semibold" >Biass AI</p>
+            <p className="text-center font-semibold " >Biass AI</p>
             <Image src={robot} alt="astronaut" className="h-96 w-44 object-cover animate-ping " />
             <p className="text-center italic " >powered by gemini api</p>
           </div>
