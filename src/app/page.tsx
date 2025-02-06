c47302c72b673f8ca5ae2e967a006c4553276080
nanahandresaputra <nanaggmu@gmail.com>
2025-02-06 14:58:08 +0700

    update version gemini

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 
       setInputMessage('');
 
-      fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyApbdgzoCvjFLksDx5OhYr3rK4XhLmDa3g', {
+      fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyApbdgzoCvjFLksDx5OhYr3rK4XhLmDa3g', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
